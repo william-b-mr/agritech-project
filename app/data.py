29ce44a717f4120adc9de9ac3a934fa2eca1d86c
William Milner <william.b.mr@gmail.com>
2023-07-28 20:30:57 -0300

    Setup basic environment and write fetch data functions and test it for different use cases

--- app/data.py
+++ app/data.py
@@ -1,18 +1,28 @@
 import pandas as pd
 from google.cloud import bigquery
 
-
+PROJECT_NAME="windproject"
+PROJECT_ID="windproject-393609"
+query = """
+    SELECT *
+    FROM `windproject-393609.FAOSTAT.crop_yield`
+    """
 
 def fetch_data(query):
     '''
     Function to fetch desired data from the main file stored in bigquery
     The query defines whether you get it all or just a desired slice
     '''
-    client = bigquery.Client(project=PROJECT_NAME)
+    client = bigquery.Client()
     query_job = client.query(query)
+    result = query_job.result()
     
-    return query_job.result().to_dataframe()
+    # Create a DataFrame from the queried results
+    data_df = result.to_dataframe()
+    
+    return data_df
 
-def clean_df(df):
-    
-    df.drop(columns = ["Domain","Domain_Code","Area_Code__M49_","Element_Code","Item_Code__CPC_","Year_Code"], inplace = True)
+if __name__=="__main__":
+    data = fetch_data(query)
+    print(data.head())
+    print(data.info())
