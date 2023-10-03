3e46031634c755798651e1db516001ea0931075d
William Milner <william.b.mr@gmail.com>
2023-10-03 18:12:28 -0300

    Created FAOSTAT function and general structure

---
+++ app/main.py
@@ -0,0 +1,14 @@
+# Imports needed
+import pandas as pd
+import numpy as np
+import os
+from data import fetch_yield_data
+
+# Generate dataframe with latest data from FAOSTAT
+
+# Predict the next x years for every country (in the world?)
+
+# Populate db in Cloud with predict data
+
+if __name__=="__main__":
+    pass
